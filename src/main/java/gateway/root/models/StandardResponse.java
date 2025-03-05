df9e4778b924edbf674478a9ed6c5512d7666a6a
ebraimsambo <ebraimsambo@gmail.com>
2025-03-05 10:41:49 +0100

    ORRO

--- src/main/java/gateway/root/models/StandardResponse.java
+++ src/main/java/gateway/root/models/StandardResponse.java
@@ -1,4 +1,18 @@
 package gateway.root.models;
 
-public class StandardResponse {
+import lombok.Data;
+
+@Data
+public class StandardResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+    private String timestamp;
+
+    public StandardResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.timestamp = java.time.LocalDateTime.now().toString();
+    }
 }
